282d72bd7985450b4424fadba70543859c299aec
junnun15 <junnunprodhan@gmail.com>
2022-11-24 01:55:57 +0600

    title added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { routes } from './Routes/Routes/Routes';
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
   return (
     <div>
       <RouterProvider router={routes} ></RouterProvider>
+      <ToastContainer />
     </div>
   );
 }
